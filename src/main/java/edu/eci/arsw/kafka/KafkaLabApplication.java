cfea0f3c880da5f23ab1265d58f582b68f0ad629
Diego Montes <diego.montes-b@mail.escuelaing.edu.co>
2026-06-30 09:21:21 -0500

    feat: configuracion de infraestructura

--- src/main/java/edu/eci/arsw/kafka/KafkaLabApplication.java
+++ src/main/java/edu/eci/arsw/kafka/KafkaLabApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KafkaEventDrivenArchitectureApplication {
+public class KafkaLabApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(KafkaEventDrivenArchitectureApplication.class, args);
+        SpringApplication.run(KafkaLabApplication.class, args);
     }
 
 }
