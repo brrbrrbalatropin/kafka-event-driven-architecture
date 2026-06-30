8813b0a63165e18f32819b5139f8a67f450eb554
Daniel Felipe Rayo Rodriguez <daniel.rayo@mail.escuelaing.edu.co>
2026-06-30 08:43:31 -0500

    push inicial

---
+++ src/main/java/edu/eci/arsw/kafka/KafkaEventDrivenArchitectureApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaEventDrivenArchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaEventDrivenArchitectureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/eci/arsw/kafka/KafkaEventDrivenArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaEventDrivenArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
